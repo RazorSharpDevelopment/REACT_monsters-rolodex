c412f9f5997d427a5b6decd2beb16287a4aabaa3
JSCoding365 <jscoding365@gmail.com>
2020-04-26 21:50:02 +0300

    Updates

---
+++ precache-manifest.bcaa10f2ecd2b1c49cdd54c29bc51235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b16f52fadeeb4cbc53e2cd525d77292",
+    "url": "/REACT_monsters-rolodex/index.html"
+  },
+  {
+    "revision": "16ca5e8639350da3b390",
+    "url": "/REACT_monsters-rolodex/static/css/main.53e7ad5a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/REACT_monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT_monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ca5e8639350da3b390",
+    "url": "/REACT_monsters-rolodex/static/js/main.717c751a.chunk.js"
+  },
+  {
+    "revision": "9d0687b20b89ef5098b2",
+    "url": "/REACT_monsters-rolodex/static/js/runtime-main.979134cd.js"
+  }
+]);
